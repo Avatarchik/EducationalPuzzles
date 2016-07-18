8b16967b189f0ff2be24459bc4f5f8207d62a137
Denis Pichurkin <reckof@bk.ru>
2016-07-18 12:43:33 +0300

    settings inspector

--- Assets/Scripts/MVC/Views/View.cs
+++ Assets/Scripts/MVC/Views/View.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class View : MonoBehaviour {
+public class View : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public ConfirmAdvertisingView ConfirmAdvertisingView;
+	public GameView GameView;
+	public MenuView MenuView;
+	public PuzzleView PuzzleView;
+	public SettingView SettingView;
 }
